009b9eea7e3a473c9a5704a16e17398aae7f94f6
Frisk-PichMir <omardio102@gmail.com>
2026-04-24 00:03:05 -0400

    Fixed item update, not sure how to adust the lighting of the item in the inventory icon

---
+++ src/main/java/net/frisk/testmod/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.frisk.testmod.item;
+
+import net.frisk.testmod.TestMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
+
+    public static final RegistryObject<Item> AWAKENED_SOUL_SHARD = ITEMS.register("awakened_soul_shard",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
